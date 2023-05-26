6072c11937452bd3fb43abddf22c09992abd6b0e
aXenDeveloper <axendeveloper@gmail.com>
2023-05-26 10:13:23 +0200

    Init

---
+++ components/Button.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import { Button } from './Button';
+import { RootMock } from '../__mocks__/RootMock';
+
+describe('Button', () => {
+  it('renders a button with the correct text', () => {
+    const { getByText } = render(<Button />, {
+      wrapper: RootMock
+    });
+    expect(getByText('Click me')).toBeInTheDocument();
+  });
+});
---
+++ components/Button.tsx
@@ -0,0 +1,5 @@
+import Link from 'next-intl/link';
+
+export const Button = () => {
+  return <Link href="/">Test</Link>;
+};
