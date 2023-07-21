849c9eb9a29cd820d6b3160af9f9ebfb00d523f8
aXenDeveloper <axendeveloper@gmail.com>
2023-07-21 13:25:32 +0200

    Change jest to vitest

--- components/Button.test.tsx
+++ components/Button.test.tsx
@@ -1,12 +1,13 @@
 import { render } from '@testing-library/react';
 import { Button } from './Button';
 import { RootMock } from '../__mocks__/RootMock';
+import { expect, describe, it } from 'vitest';
 
 describe('Button', () => {
   it('renders a button with the correct text', () => {
     const { getByText } = render(<Button />, {
       wrapper: RootMock
     });
-    expect(getByText('Click me')).toBeInTheDocument();
+    expect(getByText('Click me')).toBeDefined();
   });
 });
